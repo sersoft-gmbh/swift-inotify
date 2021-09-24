9cb862120c76848b95c4fc7afd35db5c7eea3ac7
Florian Friedrich <ffried@me.com>
2021-09-24 21:20:20 +0200

    Add Swift 5.5 support

--- Package@swift-5.4.swift
+++ Package@swift-5.4.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-system.git", .upToNextMinor(from: "0.0.2")),
-        .package(url: "https://github.com/sersoft-gmbh/swift-filestreamer.git", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/sersoft-gmbh/swift-filestreamer.git", .upToNextMinor(from: "0.2.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
