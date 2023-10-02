b0e558cee4fdc5bf274625af363bb7bf35f4a649
Florian Friedrich <ffried@me.com>
2023-10-02 11:46:14 +0200

    Fix test

--- Tests/InotifyTests/InotifyTests.swift
+++ Tests/InotifyTests/InotifyTests.swift
@@ -12,7 +12,7 @@ final class InotifyTests: XCTestCase {
         }
         let notifier = try Inotifier()
         let expectedEventCount = 3
-        let eventsTask = Task<Array<InotifyEvent>, Error>.detached {
+        let eventsTask = Task<Array<InotifyEvent>, any Error>.detached {
             var collectedEvents = Array<InotifyEvent>()
             for await event in try await notifier.events(for: FilePath(tempDir.path)) {
                 collectedEvents.append(event)
@@ -22,10 +22,18 @@ final class InotifyTests: XCTestCase {
         }
         let testFile = tempDir.appendingPathComponent("some_file.txt")
         try "some test".write(to: testFile, atomically: true, encoding: .utf8)
+        try await Task.sleep(nanoseconds: 100)
         let anotherTestFile = tempDir.appendingPathComponent("some_other_file.txt")
         try FileManager.default.moveItem(at: testFile, to: anotherTestFile)
+        try await Task.sleep(nanoseconds: 100)
         try FileManager.default.removeItem(at: anotherTestFile)
+        let cancellation = Task.detached {
+            try await Task.sleep(nanoseconds: 1_000_000_000)
+            eventsTask.cancel()
+        }
         let events = try await eventsTask.value
+        cancellation.cancel()
+        XCTAssertFalse(eventsTask.isCancelled)
         XCTAssertEqual(events.count, expectedEventCount)
     }
 }
